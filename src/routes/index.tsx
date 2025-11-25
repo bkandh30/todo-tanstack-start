e2e943a3f0cb851309ef5770df905328422c45e7
bkandh30 <kandhari.bhavya20@gmail.com>
2025-11-25 16:14:11 -0700

    Setup database

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,36 @@
+import { db } from '@/db'
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
 
-export const Route = createFileRoute('/')({ component: App })
+const serverLoader = createServerFn({ method: 'GET' }).handler(() => {
+  return db.query.todos.findMany()
+})
+
+export const Route = createFileRoute('/')({
+  component: App,
+  loader: () => {
+    return serverLoader()
+  },
+})
 
 function App() {
-  return null
+  const todos = Route.useLoaderData()
+
+  const completedCount = todos.filter((t) => t.isComplete).length
+  const totalCount = todos.length
+
+  return (
+    <div className="min-h-screen container space-y-8">
+      <div className="flex justify-between items-center gap-4">
+        <div className="space-y-2">
+          <h1 className="text-4xl font-bold">Todo List</h1>
+          {totalCount > 0 && (
+            <Badge variant="outline">
+              {completedCount} of {totalCount} completed
+            </Badge>
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
