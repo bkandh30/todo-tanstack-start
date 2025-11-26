5f6dc77e9972721a80835706d7b5edcb71fa0f5b
bkandh30 <kandhari.bhavya20@gmail.com>
2025-11-25 19:27:43 -0700

    Created Todo Table component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
-import { ListTodoIcon, PlusIcon } from 'lucide-react'
+import { ListTodoIcon, PlusIcon, EditIcon, Trash2Icon } from 'lucide-react'
 import {
   Empty,
   EmptyContent,
@@ -10,15 +10,24 @@ import {
   EmptyTitle,
 } from '@/components/ui/empty'
 import { db } from '@/db'
-import { createFileRoute, Link } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
+import { createFileRoute, Link, useRouter } from '@tanstack/react-router'
+import { createServerFn, useServerFn } from '@tanstack/react-start'
 import {
   Table,
   TableHead,
   TableHeader,
   TableBody,
   TableRow,
+  TableCell,
 } from '@/components/ui/table'
+import z from 'zod'
+import { todos } from '@/db/schema'
+import { eq } from 'drizzle-orm'
+import { startTransition, useState } from 'react'
+import { Checkbox } from '@/components/ui/checkbox'
+import { cn } from '@/lib/utils'
+import { LocalCountButton } from '@/components/local-count-button'
+import { ActionButton } from '@/components/ui/action-button'
 
 const serverLoader = createServerFn({ method: 'GET' }).handler(() => {
   return db.query.todos.findMany()
@@ -48,7 +57,8 @@ function App() {
             </Badge>
           )}
         </div>
-        <div>
+        <div className="flex gap-2">
+          <LocalCountButton />
           <Button size="sm" asChild>
             <Link to="/todos/new">
               <PlusIcon />
@@ -92,4 +102,121 @@ function TodoListTable({
       </Empty>
     )
   }
+
+  return (
+    <Table>
+      <TableHeader>
+        <Table className="hover:bg-transparent">
+          <TableHead></TableHead>
+          <TableHead>Task</TableHead>
+          <TableHead>Created On</TableHead>
+          <TableHead className="w-0"></TableHead>
+        </Table>
+      </TableHeader>
+      <TableBody>
+        {todos.map((todo) => (
+          <TodoTableRow key={todo.id} {...todo} />
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
+
+const deleteFn = createServerFn({ method: 'POST' })
+  .inputValidator(z.object({ id: z.string().min(1) }))
+  .handler(async ({ data }) => {
+    await db.delete(todos).where(eq(todos.id, data.id))
+
+    return { error: false }
+  })
+
+const toggleFn = createServerFn({ method: 'POST' })
+  .inputValidator(
+    z.object({
+      id: z.string().min(1),
+      isComplete: z.boolean(),
+    }),
+  )
+  .handler(async ({ data }) => {
+    await new Promise((res) => setTimeout(res, 1000))
+
+    await db
+      .update(todos)
+      .set({ isComplete: data.isComplete })
+      .where(eq(todos.id, data.id))
+  })
+
+function TodoTableRow({
+  createdAt,
+  id,
+  isComplete,
+  name,
+}: {
+  id: string
+  name: string
+  isComplete: boolean
+  createdAt: Date
+}) {
+  const deleteFnServer = useServerFn(deleteFn)
+  const toggleFnServer = useServerFn(toggleFn)
+  const [isCurrentComplete, setIsCurrentComplete] = useState(isComplete)
+  const router = useRouter()
+
+  return (
+    <TableRow
+      onClick={(e) => {
+        const target = e.target as HTMLElement
+        if (target.closest('[data-actions]')) return
+
+        setIsCurrentComplete((c) => !c)
+        startTransition(async () => {
+          await toggleFnServer({ data: { id, isComplete: !isCurrentComplete } })
+          router.invalidate()
+        })
+      }}
+    >
+      <TableCell>
+        <Checkbox checked={isCurrentComplete} />
+      </TableCell>
+      <TableCell
+        className={cn(
+          'font-medium',
+          isCurrentComplete && 'text-muted-foreground line-through',
+        )}
+      >
+        {name}
+      </TableCell>
+      <TableCell className="text-sm text-muted-foreground">
+        {formatDate(createdAt)}
+      </TableCell>
+      <TableCell data-actions>
+        <div className="flex items-center justify-end gap-1">
+          <Button variant="ghost" size="icon-sm" asChild>
+            <Link to="/todos/$id/edit" params={{ id }}>
+              <EditIcon />
+            </Link>
+          </Button>
+          <ActionButton
+            action={async () => {
+              const res = await deleteFnServer({ data: { id } })
+              router.invalidate()
+              return res
+            }}
+            variant="destructiveGhost"
+            size="icon-sm"
+          >
+            <Trash2Icon />
+          </ActionButton>
+        </div>
+      </TableCell>
+    </TableRow>
+  )
+}
+
+function formatDate(date: Date) {
+  const formatter = new Intl.DateTimeFormat(undefined, {
+    dateStyle: 'short',
+  })
+
+  return formatter.format(date)
 }
